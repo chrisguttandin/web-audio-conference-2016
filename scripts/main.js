99e1df5543080a9b0029cc4678a8981cd4e53038
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-04-04 11:29:25 -0400

    add angular2's browser file

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-System.register(['./components/app.js', './components/prism.js', './components/slide.js', './components/slides.js', 'angular2/platform/browser.js'], function(exports_1, context_1) {
+System.register(['./components/app.js', './components/prism.js', './components/slide.js', './components/slides.js', './angular2-platform-browser.js'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var app_1, prism_1, slide_1, slides_1, browser_1;
