cac54897f3f3fe8367e5c51bf516c5c1c8a09590
chrisguttandin <chrisguttandin@media-codings.com>
2016-07-04 16:22:18 +0000

    build page for version v0.1.12

---
+++ test/unit/components/app.js
@@ -0,0 +1,4 @@
+"use strict";
+var testing_1 = require('@angular/core/testing');
+testing_1.it('shoud execute a dummy test', function () { return testing_1.expect(true).toEqual(true); });
+//# sourceMappingURL=app.js.map
