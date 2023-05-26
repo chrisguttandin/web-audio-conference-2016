2fdea2606133fba13adbd6d26ddbeb4a638bc204
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-05-26 12:18:33 +0200

    fix: use SCSS instead of CSS

--- src/app/slides/slide-twenty-two/slide-twenty-two.component.ts
+++ src/app/slides/slide-twenty-two/slide-twenty-two.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
-    styleUrls: ['./slide-twenty-two.component.css'],
+    styleUrls: ['./slide-twenty-two.component.scss'],
     templateUrl: './slide-twenty-two.component.html'
 })
 export class SlideTwentyTwoComponent {}
